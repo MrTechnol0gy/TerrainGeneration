4e210aa2f3a160ad3bee52790e6ff66e79596f9a
Richard Clarke <113057777+MrTechnol0gy@users.noreply.github.com>
2023-11-27 17:56:26 -0400

    Update TerrainGenerator.cs
    All Bugs All The Time

--- Assets/Scripts/TerrainGenerator.cs
+++ Assets/Scripts/TerrainGenerator.cs
@@ -7,15 +7,11 @@ public class TerrainGenerator : MonoBehaviour
 {
     // The size of the terrain
     [Header("Terrain Size")]
-    public int width = 256;
-    public int height = 256;
+    public static int width = 128;
+    public static int height = 128;
     // The scale of the terrain
     [Header("Terrain Scale")]
-    public float scale = 20f;
-    // The offset of the terrain
-    [Header("Terrain Offset")]
-    public float offsetX = 100f;
-    public float offsetY = 100f;
+    public static float scale = 20f;
     // The biome of the terrain
     [Header("Terrain Biome")]
     public Biome[] biomes;
@@ -24,9 +20,11 @@ public class TerrainGenerator : MonoBehaviour
     private MeshRenderer meshRenderer;
     private Mesh terrainMesh;
 
+    // Create height map based on Perlin Noise
+    private float[,] heightMap;
+
     private Vector3[] vertices;
     private int[] triangles;
-    private Color[] colors;
 
     [System.Serializable]
     public class Biome
@@ -37,6 +35,9 @@ public class TerrainGenerator : MonoBehaviour
         public int heightModifier;
         public Color color;
     }
+
+    // Create biome map based on height map
+    private Biome[,] biomeMap = new Biome[width, height];
 
     void Start()
     {
@@ -53,8 +54,87 @@ public class TerrainGenerator : MonoBehaviour
             meshRenderer = gameObject.AddComponent<MeshRenderer>();  // Add MeshRenderer component if not already present
         }
 
-
+        
+        // Create an array to store biome information
+        biomeMap = new Biome[width, height];
+        // Generate the height map using Perlin Noise
+        heightMap = GeneratePerlinNoiseMap(width, height, scale);
+        // Assign biomes to the terrain based on the height map
+        AssignBiomes(heightMap);
+        // Create a texture based on the biome map
+        meshRenderer.material.mainTexture = CreateBiomeTexture();
+        // Generate the terrain mesh
         GenerateTerrain();
+    }
+
+    // Generate the height map using Perlin Noise
+    public static float[,] GeneratePerlinNoiseMap(int width, int height, float scale)
+    {
+        float[,] noiseMap = new float[width, height];
+
+        // Loop through each pixel of the map
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                // Calculate sample indices based on the scale
+                float sampleX = x / scale;
+                float sampleY = y / scale;
+
+                // Generate the noise value using Perlin Noise
+                float realPerlinValue = UnityPerlinToRealPerlin(Mathf.PerlinNoise(sampleX, sampleY));
+
+                // Set the noise map value to the perlin value
+                noiseMap[x, y] = realPerlinValue;
+            }
+        }
+
+        return noiseMap;
+    }
+
+    // Assign biomes to the terrain based on the height map
+    public void AssignBiomes(float[,] heightMap)
+    {
+        // Loop through each pixel of the map
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                // Loop through each biome
+                for (int i = 0; i < biomes.Length; i++)
+                {
+                    // If the height map value is between the min and max height of the biome
+                    if (heightMap[x, y] >= biomes[i].minHeight && heightMap[x, y] <= biomes[i].maxHeight)
+                    {
+                        // Assign the biome to the pixel
+                        biomeMap[x, y] = biomes[i];
+                    }
+                }
+            }
+        }
+    }
+
+    // Create a texture based on the biome map
+    public Texture2D CreateBiomeTexture()
+    {
+        Texture2D texture = new Texture2D(width + 1, height + 1);
+
+        // Loop through each pixel of the map
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                Debug.Log(biomeMap[x, y]);
+                Biome currentBiome = biomeMap[x, y];
+                // Set the pixel color to the biome color
+                Debug.Log(currentBiome.color);
+                texture.SetPixel(x, y, currentBiome.color);
+            }
+        }
+
+        texture.Apply();
+
+        return texture;
     }
 
     void GenerateTerrain()
@@ -67,12 +147,6 @@ public class TerrainGenerator : MonoBehaviour
         {
             Debug.Log("Mesh filter doesn't have a mesh");
         }
-
-        // Create a texture to store the colours
-        Texture2D texture = new Texture2D(width, height);
-
-        Debug.Log(terrainMesh.vertices.Length);
-
     }
 
     Mesh CreateTerrainMesh()
@@ -87,7 +161,7 @@ public class TerrainGenerator : MonoBehaviour
         {
             for (int x = 0; x <= width; x++)
             {
-                float heightValue = Mathf.PerlinNoise((x + offsetX) / scale, (y + offsetY) / scale);
+                float heightValue = UnityPerlinToRealPerlin(Mathf.PerlinNoise(x  / scale, y / scale));
                 Debug.Log("Height value: " + heightValue);
 
                 vertices[vertexIndex] = new Vector3(x, heightValue, y);
@@ -113,4 +187,11 @@ public class TerrainGenerator : MonoBehaviour
 
         return mesh;
     }
+
+    public static float UnityPerlinToRealPerlin(float unityPerlin)
+    {
+        // Map Unity's Perlin range (0 to 1) to the range of real Perlin Noise (-1 to 1)
+        return unityPerlin * 2 - 1;
+    }
+
 }
